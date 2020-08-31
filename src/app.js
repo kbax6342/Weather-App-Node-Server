cc4cf4f4562cfa202a0817456046641dee2983b2
kbax6342 <k3vin.baxt3r@gmail.com>
2020-08-30 20:41:25 -0400

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 //app is the application being initiliazed
 const app = express()
+const port = process.env.PORT || 3000
 
 //Mapping to the files
 //Define paths for express config
@@ -115,7 +116,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
-//app.com
